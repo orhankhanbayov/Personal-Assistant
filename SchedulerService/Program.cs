6c703143e173fc20c45b9478e83e5d553f70c7fa
Orhan Khanbayov <orhankhanbayov@Orhans-MacBook-Air.local>
2025-03-03 20:35:30 +0000

    updated scheduler

--- SchedulerService/Program.cs
+++ SchedulerService/Program.cs
@@ -6,12 +6,11 @@ var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
 
 GlobalConfiguration.Configuration.UseSqlServerStorage(
-	"Server=sqlserver,1433;Initial Catalog=PersonalAssistantHangFire;User ID=sa;Password=MyStr0ngP@ss!;TrustServerCertificate=True;Encrypt=True;"
-);
+	builder.Configuration.GetConnectionString("DefaultConnection"));
 
 builder.Services.AddHangfire(configuration =>
 	configuration
-		// .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+		.SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
 		.UseSimpleAssemblyNameTypeSerializer()
 		.UseRecommendedSerializerSettings()
 );
@@ -21,41 +20,42 @@ builder.Services.AddLogging(builder => builder.AddConsole());
 builder.Services.AddHangfireServer();
 builder.Services.AddAutoMapper(typeof(MapperService));
 
-var grpcDatabaseService = builder.Configuration["GRPC:DatabaseService"];
-var grpcTwilioService = builder.Configuration["GRPC:TwilioService"];
+var grpcDatabaseClientAddress = builder.Configuration["GRPC:DatabaseClient"];
+var grpcTwilioClientAddress = builder.Configuration["GRPC:TwilioClient"];
 
-if (string.IsNullOrEmpty(grpcDatabaseService) || string.IsNullOrEmpty
-	(grpcTwilioService))
+
+if (string.IsNullOrEmpty(grpcDatabaseClientAddress) || string.IsNullOrEmpty
+	(grpcTwilioClientAddress))
 {
 	var missingConfigs = new List<string>();
-	if (string.IsNullOrEmpty(grpcDatabaseService)) missingConfigs.Add("GRPC:DatabaseClient");
-	if (string.IsNullOrEmpty(grpcTwilioService)) missingConfigs.Add("GRPC:TwilioClient");
+	if (string.IsNullOrEmpty(grpcDatabaseClientAddress)) missingConfigs.Add("GRPC:DatabaseClient");
+	if (string.IsNullOrEmpty(grpcTwilioClientAddress)) missingConfigs.Add("GRPC:TwilioClient");
 
 	throw new Exception($"Configuration is missing or null for: {string.Join(", ", missingConfigs)}. Exiting application.");
 }
 
 builder.Services.AddGrpcClient<Users.UsersClient>(options =>
 {
-	options.Address = new Uri(grpcDatabaseService);
+	options.Address = new Uri(grpcDatabaseClientAddress);
 });
 
 builder.Services.AddGrpcClient<Notifications.NotificationsClient>(options =>
 {
-	options.Address = new Uri(grpcDatabaseService);
+	options.Address = new Uri(grpcDatabaseClientAddress);
 });
 builder.Services.AddGrpcClient<Tasks.TasksClient>(options =>
 {
-	options.Address = new Uri(grpcDatabaseService);
+	options.Address = new Uri(grpcDatabaseClientAddress);
 });
 
 builder.Services.AddGrpcClient<Events.EventsClient>(options =>
 {
-	options.Address = new Uri(grpcDatabaseService);
+	options.Address = new Uri(grpcDatabaseClientAddress);
 });
 
 builder.Services.AddGrpcClient<TwilioPhone.TwilioPhoneClient>(options =>
 {
-	options.Address = new Uri(grpcTwilioService);
+	options.Address = new Uri(grpcTwilioClientAddress);
 });
 var host = builder.Build();
 
